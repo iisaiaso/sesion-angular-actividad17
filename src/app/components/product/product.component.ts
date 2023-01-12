204235305aa27af30c1c41a727c75bc8ce984c64
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-12 14:09:56 -0500

    agregando registro a la tabla

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class ProductComponent {
 
   productos = [
-    [1,'Galleta chokis',10.00,15.00,5,100.00],
-    [2,'Mermelada Fresa',10.00,15.00,5,100.00],
-    [3,'Aceite',10.00,15.00,5,100.00],
-    [4,'Palomita de Maiz',10.00,15.00,5,100.00],
-    [5,'Doritos',10.00,15.00,5,100.00]
+    [1, 'Galleta chokis', 10.00, 15.00, 5, 100.00],
+    [2, 'Mermelada Fresa', 65.00, 80.00, 15, 100.00],
+    [3, 'Aceite', 18.00, 20.00, 2, 100.00],
+    [4, 'Palomita de Maiz', 12.00, 15.00, 3, 100.00],
+    [5, 'Doritos', 5.00, 8.00, 3, 100.00]
   ]
 }
