4b159c1827056cdf73b6f8de6ba9b41fe238ae54
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-13 07:40:59 -0500

    Init de proyecto concluido

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,20 +1,25 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HearderComponent } from './hearder/hearder.component';
 
 
 
 @NgModule({
   declarations: [
     ProductComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    HearderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
-    ProductComponent
+    ProductComponent,
+    HearderComponent
   ]
 })
 export class ComponentsModule { }
